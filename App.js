ac534d47ced75fe3f45e221a6f2057f62a33db89
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-05-20 21:48:22 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, TouchableOpacity, Text, View, FlatList } from "react-native";
-import {AntDesign} from "@expo/vector-icons"
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  FlatList,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import colors from "./Colors";
-import tempData from "./TempData"
+import tempData from "./TempData";
+import TaskitList from "./components/TaskitList";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
         <View style={{ flexDirection: "row" }}>
           <View style={styles.divider} />
-            <Text style={styles.title}>
-              TASK <Text style={{ fontWeight: "300", color: colors.blue }}>-IT</Text>
-            </Text>
+          <Text style={styles.title}>
+            TASK{" "}
+            <Text style={{ fontWeight: "300", color: colors.blue }}>-IT</Text>
+          </Text>
           <View style={styles.divider} />
         </View>
 
@@ -26,16 +33,17 @@ export default class App extends React.Component {
           <Text style={styles.add}>Add List</Text>
         </View>
 
-        <View style={{height: 275, paddingLeft: 32}}>
-          <FlatList 
-            data={tempData} 
-            keyExtractor={item => item.name} 
-            horizontal={true} 
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={(item) => item.name}
+            horizontal={true}
             showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => <View><Text> {item.name} </Text></View>}  
+            renderItem={({ item }) => (
+              <TaskitList list={item} />
+            )}
           />
         </View>
-
       </View>
     );
   }
@@ -73,5 +81,5 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     fontSize: 14,
     marginTop: 8,
-  }
+  },
 });
