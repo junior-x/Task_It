6e7f8f46e079a42f6e459ee152a472a0c81e3b37
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-05-20 21:52:44 -0300

    Add files via upload

--- components/TaskitList.js
+++ components/TaskitList.js
@@ -3,8 +3,21 @@ import {StyleSheet, Text, View, } from "react-native";
 
 export default TaskitList = ({list}) => {
     return(
-        <View>
-            <Text>{list.name}</Text>
+        <View style={[Styles.listContainer, { backgroundColor: list.color}]}>
+            <Text style={styles.listTitle} numberOfLines={1}>
+                {list.name}
+            </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 16,
+        borderRadius: 6,
+        marginHorizontal: 12,
+        alignItems: "center",
+        width: 200,
+    },
+})
