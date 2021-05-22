127ec00f4582c0cdb79919c0426de266d08f8c65
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-05-22 17:02:05 -0300

    Add files via upload

--- components/TaskitList.js
+++ components/TaskitList.js
@@ -1,12 +1,29 @@
 import React from "react";
 import {StyleSheet, Text, View, } from "react-native";
+import colors from "../Colors";
 
 export default TaskitList = ({list}) => {
+    const completedCount = list.todos.filter(todo => todo.completed).length;
+    const remainingCount = list.todos.length - completedCount;
+
     return(
         <View style={[styles.listContainer, { backgroundColor: list.color}]}>
             <Text style={styles.listTitle} numberOfLines={1}>
                 {list.name}
             </Text>
+            
+            <View>
+                <View style={{ alignItems: "center"}}>
+                    <Text style={styles.count}>{remainingCount}</Text>
+                    <Text style={styles.subtitle}>Remaining</Text>
+                </View>
+
+                <View style={{ alignItems: "center" }}>
+                    <Text style={styles.count}>{completedCount}</Text>
+                    <Text style={styles.subtitle}>Completed</Text>
+                </View>
+            </View>
+
         </View>
     );
 }
@@ -19,5 +36,21 @@ const styles = StyleSheet.create({
         marginHorizontal: 12,
         alignItems: "center",
         width: 200,
+    },
+    listTitle: {
+        fontSize: 24,
+        fontWeight: "700",
+        color: colors.dark,
+        marginBottom: 18,
+    },
+    count: {
+        fontSize: 48,
+        fontWeight: "200",
+        color: colors.light,
+    },
+    subtitle: {
+        fontSize: 12,
+        fontWeight: "700",
+        color: colors.dark,
     },
 })
