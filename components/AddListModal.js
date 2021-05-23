2a6c38c8bfb1e7cc0d1d3180fcc6f96f9e2bdd6e
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-05-22 23:59:13 -0300

    Bug Navigation Fixed

--- components/AddListModal.js
+++ components/AddListModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableOpacity, TextInput } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import colors from "../Colors";
 
@@ -11,13 +11,14 @@ export default class AddListModal extends React.Component {
                 <TouchableOpacity 
                 style={{position: "absolute", top: 64, right: 32}} 
                 onPress={this.props.closeModal} >
-                    <AntDesign name="close" size={24} color={colors.light} />
+                    <AntDesign name="close" size={24} color={colors.dark} />
                 </TouchableOpacity>
 
                 <View style={{alignSelf: "stretch", marginHorizontal: 32}}>
                     <Text style={styles.title}>
                         Create Task List
                     </Text>
+                    <TextInput style={styles.input} placeholder="Task Name?" />
                 </View>
 
             </KeyboardAvoidingView>
@@ -27,8 +28,16 @@ export default class AddListModal extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        color: colors.dark,
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "800",
+        color: colors.dark,
+        alignSelf: "center",
+        marginBottom: 16,
     },
 });
